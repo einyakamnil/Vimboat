2b8b48105407590ff83e958796d8a37f8773ac60
EinYakAmNil <einyak@gmail.com>
2023-03-03 23:48:21 +0100

    testing queries

--- src/python/db.py
+++ src/python/db.py
@@ -1,5 +1,6 @@
 import apsw
 
+
 conn = apsw.Connection(
     "../../share/vimboat/cache.db",
     flags=apsw.SQLITE_OPEN_READONLY
@@ -13,12 +14,26 @@ feed_cols = conn.execute(
 
 #for i in list(feed_cols):
 #    print(i)
-
+#
 #for i in list(item_cols):
 #    print(i)
-feeds = conn.execute(
-    "SELECT title,etag FROM rss_feed WHERE LENGTH(etag) > 0"
+#
+#feeds = conn.execute(
+#    "SELECT title,rssurl FROM rss_feed"
+#)
+
+kammer = next(
+    conn.execute(
+        "SELECT rssurl FROM rss_feed WHERE title = 'Kammerbild'"
+    )
+)[0]
+
+items = list(
+    conn.execute(
+        f"SELECT Count(*) FROM rss_item WHERE feedurl = '{kammer}' AND unread = 1"
+    )
 )
 
-for f in list(feeds):
-    print(f)
+print(len(items))
+for i in items:
+    print(i)
